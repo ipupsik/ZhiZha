a9ce1c10951c483ca84e41ae6d5ff78d409d6d8a
Vladimir Nikitin <potryas85@mail.ru>
2021-05-03 01:13:56 +0300

    Launch simple window

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 using sf::Window;
 using sf::Event;
 using sf::VideoMode;
 
 int main() {
-    auto window = Window(VideoMode(1080, 1080 * 9 / 16), "Sample");
+    sf::RenderWindow window(VideoMode(1080, 1080 * 9 / 16), "Sample");
+
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event)) {
-            if (event.type == Event::Closed)
+            if (event.type == Event::Closed) {
                 window.close();
-            else if (event.type == Event::KeyPressed)
+                return 0;
+            } else if (event.type == Event::KeyPressed) {
                 std::cout << event.key.code << std::endl;
+            }
         }
+        window.clear(sf::Color::White);
+        window.display();
+        sf::sleep(sf::seconds(1.f / 60));
     }
 }
