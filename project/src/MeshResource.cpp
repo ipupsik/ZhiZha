814b0557cd9c490af7b77f9caa0771a44f7f8d6c
Владимир <nikitinvl@student.bmstu.ru>
2021-05-12 14:21:48 +0300

    Glad

---
+++ project/src/MeshResource.cpp
@@ -0,0 +1,134 @@
+#include "MeshResource.h"
+
+#include <fstream>
+#include <iostream>
+
+#include "glad/glad.h"
+#include "SFML/OpenGL.hpp"
+
+void MeshResource::readFile(std::string&& filename) {
+    std::string s;
+    s = "Static Meshes/";
+    s += filename;
+    s += ".obj";
+    std::ifstream fin(s);
+    if (!fin)
+        return;
+    while (fin >> s)
+    {
+        if (s == "v")
+        {
+            sf::Vector2f v;
+            fin >> v.x >> v.y;
+
+            float tmp;
+            fin >> tmp;
+
+            _vertices.push_back(v);
+        }
+        else if (s == "f")
+        {
+            char symbol;
+            Face f, n, uv;
+
+            fin >> f.v1;
+            fin >> symbol;
+            fin >> uv.v1;
+            fin >> symbol;
+            fin >> n.v1;
+
+            fin >> f.v2;
+            fin >> symbol;
+            fin >> uv.v2;
+            fin >> symbol;
+            fin >> n.v2;
+
+            fin >> f.v3;
+            fin >> symbol;
+            fin >> uv.v3;
+            fin >> symbol;
+            fin >> n.v3;
+
+            f.v1--;
+            f.v2--;
+            f.v3--;
+
+            uv.v1--;
+            uv.v2--;
+            uv.v3--;
+
+            _faces.push_back(f);
+            _facesNormal.push_back(n);
+            _facesTexCoord.push_back(uv);
+        }
+        else if (s == "vn")
+        {
+            sf::Vector2f vn;
+            fin >> vn.x >> vn.y;
+
+            float tmp;
+            fin >> tmp;
+
+            _normals.push_back(vn);
+        }
+        else if (s == "vt")
+        {
+            sf::Vector2f vt;
+            fin >> vt.x >> vt.y;
+            _uvsInit.push_back({ 1.0f - vt.x, vt.y });
+        }
+        else
+        {
+            std::string tmp;
+            getline(fin, tmp);
+        }
+    }
+    std::cout << s;
+    _uvs.resize(_uvsInit.size());
+    for (int i = 0; i < _faces.size(); i++)
+    {
+        _uvs[_faces[i].v1] = _uvsInit[_facesTexCoord[i].v1];
+        _uvs[_faces[i].v2] = _uvsInit[_facesTexCoord[i].v2];
+        _uvs[_faces[i].v3] = _uvsInit[_facesTexCoord[i].v3];
+    }
+}
+
+void MeshResource::initMesh() {
+    glGenBuffers(1, &_indexVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, _indexVBO);
+    {
+        glBufferData(GL_ARRAY_BUFFER, sizeof(sf::Vector2f) * _vertices.size(), _vertices.begin()._Ptr, GL_STATIC_DRAW);
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenBuffers(1, &_indexTexVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, _indexTexVBO);
+    {
+        glBufferData(GL_ARRAY_BUFFER, sizeof(sf::Vector2f) * _uvs.size(), _uvs.begin()._Ptr, GL_STATIC_DRAW);
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenBuffers(1, &_indexEBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexEBO);
+    {
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Face) * _faces.size(), _faces.begin()._Ptr, GL_STATIC_DRAW);
+    }
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glGenVertexArrays(1, &_indexVAO);
+    glBindVertexArray(_indexVAO);
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, _indexVBO);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, _indexTexVBO);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_TRUE, 0, NULL);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    glBindVertexArray(0);
+}
+
+MeshResource::MeshResource(std::string&& filename) {
+    readFile(std::move(filename));
+    initMesh();
+}
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,58 +1,10 @@
-#include <thread>
-#include <iostream>
-
+#include "SFML/OpenGL.hpp"
+#include "MeshResource.h"
+#include "glad/glad.h"
 #include "utils.h"
-#include "SystemManager.h"
-#include "Systems.h"
-#include "SFML/Graphics/RenderWindow.hpp"
-#include "SFML/Window/Event.hpp"
 
 using namespace sf::Extensions::Vector2;
 
 int main() {
-	auto window = sf::RenderWindow(sf::VideoMode(800, 600), "Sample");
-	float delta = 0;
-
-	auto& sys = SystemManager::Current
-		.RegisterSystem<RenderSystem>(window)
-		.RegisterSystem<TestSystem>(0.016)
-		.RegisterSystem<EventSystem>(window);
-
-	window.setActive(false);
-
-	auto render = std::thread([&window, &sys, &delta] {
-		sf::Clock clock{};
-		window.setActive(true);
-
-		while (window.isOpen()) {
-			sys.Update();
-
-			sys.PostUpdate();
-			
-			delta = 1 / clock.restart().asSeconds();
-		}
-		std::cout << std::endl;
-		window.setActive(false);
-	});
-
-	auto physic = std::thread([&window, &sys, &delta] {
-		sf::Clock deltaClock;
-		const sf::Time fixedDelta = sf::seconds(1.0f / 60);
-		sf::Time sinceUpdate = sf::Time::Zero;
-
-		while (window.isOpen()) {
-			sinceUpdate += deltaClock.restart();
-
-			if (sinceUpdate > fixedDelta) {
-				sinceUpdate -= fixedDelta;
-				sys.FixedUpdate();
-				std::cout << "FPS: " << delta << "\r";
-			}
-		}
-	});
-	
-	sys.PostInit();
-	render.join();
-	window.setActive(true);
-	physic.join();
+	MeshResource res("Circle");
 }
