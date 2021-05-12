7e8b04f989eea8f3f25261abbf9d40eadffff06c
Владимир <nikitinvl@student.bmstu.ru>
2021-05-12 14:39:55 +0300

    Glad further

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,10 +1,10 @@
-#include "SFML/OpenGL.hpp"
 #include "MeshResource.h"
-#include "glad/glad.h"
 #include "utils.h"
+#include "glad/glad.h"
 
 using namespace sf::Extensions::Vector2;
 
 int main() {
+	gladLoadGL();
 	MeshResource res("Circle");
 }
