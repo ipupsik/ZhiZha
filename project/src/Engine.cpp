31cf11a6f2bb69f0994148d7f9843eb6d1ccb8b6
Владимир <nikitinvl@student.bmstu.ru>
2021-05-13 23:32:22 +0300

    Added engine wrapper

---
+++ project/src/Engine.cpp
@@ -0,0 +1,39 @@
+#include "Engine.h"
+
+void Engine::initRenderThread() {
+	sf::Clock clock{};
+	_window.setActive(true);
+
+	while(_window.isOpen()) {
+		_systemManager.Update();
+		_systemManager.PostUpdate();
+		_deltaTime = clock.restart().asSeconds();
+	}
+
+	_window.setActive(false);
+}
+
+void Engine::initFixedUpdateThread() const {
+	const sf::Time fixedTime = sf::seconds(1 / 30.0f);
+	sf::Clock deltaClock{};
+	sf::Time sinceUpdate = sf::Time::Zero;
+
+	while(_window.isOpen()) {
+		sinceUpdate += deltaClock.restart();
+
+		if (sinceUpdate > fixedTime) {
+			sinceUpdate -= fixedTime;
+			_systemManager.FixedUpdate();
+		}
+	}
+}
+
+void Engine::Start() {
+	std::thread update([&] { initRenderThread(); });
+	std::thread fixed([&] { initFixedUpdateThread(); });
+
+	_systemManager.PostInit();
+	update.join();
+	_window.setActive(true);
+	fixed.join();
+}
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,18 +1,10 @@
-#include <chrono>
-
-#include "ResourceFile.h"
-#include "MeshResource.h"
+#include "Engine.h"
 #include "TextureResource.h"
-#include "ShaderResource.h"
 #include "SFML/Window/Window.hpp"
-
-using namespace std::chrono_literals;
 
 int main() {
 	sf::Window window(sf::VideoMode(800, 600), "Sample");
 	gladLoadGL();
 	
-	MeshResource res("Circle.obj");
-	TextureResource res2("Circle_Albedo.png");
-	ShaderResource res3("Circle");
+	Engine(std::move(window)).Start();
 }
