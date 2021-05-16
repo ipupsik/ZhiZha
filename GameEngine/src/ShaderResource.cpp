83891ee6126381cf793c2fa7f92e9656619059b3
KrUtYaIII <burdunin.gosha@yandex.ru>
2021-05-16 18:00:17 +0300

    Actor + Material + Some Systems

--- GameEngine/src/ShaderResource.cpp
+++ GameEngine/src/ShaderResource.cpp
@@ -4,8 +4,6 @@
 
 #include <sstream>
 
-constexpr auto BUFFER = 2048;
-
 const std::string& LoadShaderFile(std::string&& filename)
 {
 	std::string s = "Materials/";
@@ -22,20 +20,28 @@ const std::string& LoadShaderFile(std::string&& filename)
 	return filetext;
 }
 
-GLuint ShaderResource::LoadShader(GLuint type) {
+void ShaderResource::LoadShader(GLuint type) {
 	const std::string& shaderProgram = LoadShaderFile(std::move(Name()));
 	const char* text = shaderProgram.c_str();
-	const GLuint shader = glCreateShader(type);
-	glShaderSource(shader, 1, &text, nullptr);
-	glCompileShader(shader);
-
-	return shader;
+	_shaderId = glCreateShader(type);
+	glShaderSource(_shaderId, 1, &text, nullptr);
+	glCompileShader(_shaderId);
 }
 
 VertexShaderResource::VertexShaderResource(std::string&& shaderName) : ShaderResource(shaderName + ".vert")
 {
 }
 
+void VertexShaderResource::LoadShader(GLuint type)
+{
+	ShaderResource::LoadShader(GL_VERTEX_SHADER);
+}
+
 FragmentShaderResource::FragmentShaderResource(std::string&& shaderName) : ShaderResource(shaderName + ".frag")
 {
+}
+
+void FragmentShaderResource::LoadShader(GLuint type)
+{
+	ShaderResource::LoadShader(GL_FRAGMENT_SHADER);
 }
