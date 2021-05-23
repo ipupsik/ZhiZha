e91a3a06f237f19575579a59c8fcace20c5b4bfe
Vladimir Nikitin <potryas85@mail.ru>
2021-05-24 02:24:34 +0300

    Proper includes; define -> constexpr; little fixes

--- PhysicsEngine/src/CollisionSystem.cpp
+++ PhysicsEngine/src/CollisionSystem.cpp
@@ -1,50 +1,45 @@
-#include "System.h"
-#include "Components/TransformComponent.h"
-#include "GameTime.h"
-#include "../PhysicsEngine/include/ComponentDrop.h"
+#include <Components/TransformComponent.h>
+#include "ComponentDrop.h"
 #include "Components/CircleCollisionComponent.h"
 #include "utils.h"
-#include "../PhysicsEngine/include/DefinesPhysics.h"
+#include "DefinesPhysics.h"
 
 #include "Systems/CollisionSystem.h"
 
 using namespace sf::Extensions::Vector2;
 
 void CollisionSystem::OnFixedUpdate() {
-	//нашли соседей шариков -- start
-	const auto& items = _entities->GetEntitiesBy<ComponentDrop>();
+	//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ -- start
+	const auto& items = _entities->GetEntitiesBy<ComponentDrop, TransformComponent>();
 
-	for (auto& [components, current_entity] : items)
-	{
-		auto& [drop_current] = components;
-		for (auto& [components, other_entity] : items)
-		{
-			auto& [drop_other] = components;
-			if (drop_current->Position == drop_other->Position)
-			{
+	for (auto&[components, current_entity] : items) {
+		auto&[drop_current, transformCurrent] = components;
+		for (auto&[components, other_entity] : items) {
+			auto&[drop_other, transformOther] = components;
+			if (transformCurrent->Location == transformOther->Location) {
 				continue;
 			}
 
-			if (drop_current->Position->*Magnitude(drop_other->Position) <= 2 * RADIUS)
-			{
+			// here use squared distance, because to get square is much easier, then to get square
+			if (transformCurrent->Location->*SqrMagnitude(transformOther->Location)
+					<= 4 * RADIUS * RADIUS) {
 				drop_current->neighbours.push_back(other_entity);
 			}
-
 		}
 	}
-	//нашли соседей шариков -- end
+	//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ -- end
 
-	//находим векторы соседи_импакт -- start
-	for (auto& [components, current_entity] : items)
-	{
-		auto& [drop_current] = components;
+	//пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ_пїЅпїЅпїЅпїЅпїЅпїЅ -- start
+	for (auto&[components, current_entity] : items) {
+		auto&[drop, transform] = components;
 
-		drop_current->Neighbour_impact = { 0, 0 };
+		drop->Neighbour_impact = { 0, 0 };
 
-		for (int i = 0; i < drop_current->neighbours.size(); ++i)
-		{
-			drop_current->Neighbour_impact += -_entities->GetComponent<ComponentDrop>(*(drop_current->neighbours[i]))->Position + drop_current->Position;
+		for (int i = 0; i < drop->neighbours.size(); ++i) {
+			drop->Neighbour_impact +=
+					-_entities->GetComponent<TransformComponent>(*(drop->neighbours[i]))->Location
+							+ transform->Location;
 		}
 	}
-	//находим векторы соседи_импакт -- end
+	//пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ_пїЅпїЅпїЅпїЅпїЅпїЅ -- end
 }
