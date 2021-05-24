6ea256b52fae0814521b237b20806b2b33cdc4b0
zenya2602 <zenya.konyaev@mail.ru>
2021-05-24 19:30:32 +0300

    add speed system and clear system

---
+++ PhysicsEngine/src/ResetParamsSystem.cpp
@@ -0,0 +1,28 @@
+#include "ComponentDrop.h"
+#include "ComponentVolume.h"
+
+#include "Systems/ResetParamsSystem.h"
+
+void ResetParamsSystem::OnFixedUpdate()
+{
+	const auto& drops = _entities->GetEntitiesBy<ComponentDrop>();
+
+	for (auto& [components, cur_entity] : drops)
+	{
+		auto&[drop_comp] = components;
+
+		drop_comp->neighbours.resize(0);
+
+		drop_comp->is_force_calculated = false;
+		drop_comp->is_in_queue = false;
+		drop_comp->is_in_volume = false;
+		drop_comp->is_moved = false;
+	}
+
+	const auto& volumes = _entities->GetEntitiesBy<ComponentVolume>();
+
+	for (auto& [components, cur_entity] : volumes)
+	{
+		_entities->DestroyEntity(*cur_entity);
+	}
+}
