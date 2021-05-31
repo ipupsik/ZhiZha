f66854df11bb95d09db5dce0c720a467b2383852
EremeyR <hungry.green.bee@gmail.com>
2021-05-31 21:28:18 +0300

    definesPhisics-was-changed

--- PhysicsEngine/include/DefinesPhysics.h
+++ PhysicsEngine/include/DefinesPhysics.h
@@ -1,7 +1,7 @@
 #pragma once
 
-constexpr auto MASS = 0.001;  //  ���������� ���� ����������
+constexpr auto MASS = 0.0005;  //  ���������� ���� ����������
 constexpr auto G = 9.81;  //  �������������� ����������
-constexpr auto RADIUS = 10;  //  ������ ��������
+constexpr auto RADIUS = 0.01f;  //  ������ ��������
 constexpr auto SURFACE_FORCE = 0.25f;  //  ���������� ���������� �������
 constexpr auto DENSITY = 1000;  //  ���������� ���� �� ��������
