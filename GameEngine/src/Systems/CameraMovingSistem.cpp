732f5f0be44148ad880bce902f0ec52fe7798b94
Vladimir Nikitin <potryas85@mail.ru>
2021-06-02 01:27:11 +0300

    Smooth camera moving

--- GameEngine/src/Systems/CameraMovingSistem.cpp
+++ GameEngine/src/Systems/CameraMovingSistem.cpp
@@ -14,20 +14,18 @@ void CameraMovingSystem::OnFixedUpdate() {
 	sf::Vector2f _newLocation = { 0, 0 };
 	int good_count = 0;
 	for (auto& item : items) {
-		auto& [currentTransform, currentDrop] = item.Components;
+		auto&[currentTransform, currentDrop] = item.Components;
 		sf::Vector2f screen_location = currentTransform->Location - _camera_location;
 		if (screen_location.x <= 2.6f && screen_location.x >= -2.6f)
-			if (screen_location.y <= 2.6f && screen_location.y >= -2.6f)
-			{
+			if (screen_location.y <= 2.6f && screen_location.y >= -2.6f) {
 				_newLocation += currentTransform->Location;
 				good_count++;
 			}
 	}
-	if (good_count > 0)
-	{
+	if (good_count > 0) {
 		_newLocation /= (float)good_count;
 		_newLocation *= -1.f;
-		_camera_location = _newLocation;
+		_camera_location = (_camera_location + _newLocation) * (1 / 2.f);
 	}
 
 	//std::cout << " camera x: " << _camera_location.x << " y: " << _camera_location.y << std::endl;
