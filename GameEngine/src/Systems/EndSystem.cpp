f0db6e9f38e8c53b89f31dde854f3d4beb1b6ad4
EremeyR <hungry.green.bee@gmail.com>
2021-06-03 17:40:34 +0300

    exit condition was added

--- GameEngine/src/Systems/EndSystem.cpp
+++ GameEngine/src/Systems/EndSystem.cpp
@@ -8,15 +8,19 @@
 void EndSystem::OnFixedUpdate() {
 	//����� ������� ������� -- start
 	const auto& items = _entities->GetEntitiesBy<ComponentDrop, TransformComponent>();
-
 	for (auto& [components, current_entity] : items) {
 		auto& [drop_current, transformCurrent] = components;
-		if (transformCurrent->Location.y > 2) {
-			//transformCurrent->Scale = { 0.03, 0.03 };
+		if (transformCurrent->Location.x > 6.35) {
+			transformCurrent->Scale = { 0.03, 0.03 };
 		}
-		if (transformCurrent->Location.y > 2.2) {
-			//_entities->DestroyEntity(*current_entity);
+		if (transformCurrent->Location.x > 7) {
+			_entities->DestroyEntity(*current_entity);
+			// Win
 		}
+	}
+
+	if (items.size() == 0) {
+		// Lose
 	}
 	
 	if (_engine.GetTime().Time() > 70 && !_isOver) {
